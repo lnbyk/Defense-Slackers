29fac5d763ad379bf65d440b3f8dc3b70835efe8
Rubai <lnbyk@udel.edu>
2020-02-18 00:15:30 -0500

    adding iconfor tower selection menu

--- script/controller.js
+++ script/controller.js
@@ -10,18 +10,51 @@ $(function () {
     });
 
     /* build tower button */
-    $("#tBtn0").click(function(){
-        var position = $("#tBtn0").position();
-        var width = $( window ).width() * 0.12;
-        var height = $( window ).height() * 0.18;
-        $("#towerSImage").css({top: position.top - height / 2, left: position.left - width / 2, position:'absolute'});
-        $('#towerSelection').fadeIn('fast', function() {
-            $("#closeTowerSelection").css({top: position.top - height, left: position.left + width / 2, position:'absolute'});
+    $(".tbtn").click(function () {
+        var position = $(this).position();
+        var width = $(window).width() * 0.12;
+        var height = $(window).height() * 0.18;
+        $("#towerSImage").css({
+            top: position.top - height / 2,
+            left: position.left - width / 2,
+            position: 'absolute'
+        });
+        $('#towerSelection').fadeIn('fast', function () {
+            $("#closeTowerSelection").css({
+                top: position.top - height,
+                left: position.left + width / 2,
+                position: 'absolute'
+            });
+            $("#tower0").css({
+                top: position.top - height * 0.4,
+                left: position.left - width * 0.8,
+                position: 'absolute'
+            });
+            $("#tower1").css({
+                top: position.top - height * 0.4,
+                left: position.left - width * 0.15,
+                position: 'absolute'
+            });
+            $("#tower2").css({
+                top: position.top + height * 0.2,
+                left: position.left - width * 0.8,
+                position: 'absolute'
+            });
+            $("#tower3").css({
+                top: position.top + height * 0.2,
+                left: position.left - width * 0.12,
+                position: 'absolute'
+            });
+            $(".towerIcon").fadeIn();
+            $("#closeTowerSelection").fadeIn();
         });
     });
 
     /* button use to close tower selection menu */
-    $("#closeTowerSelection").click(function(){
+    $("#closeTowerSelection").click(function () {
         $("#towerSelection").fadeOut('fast');
+        $(".towerIcon").hide();
+        $("#closeTowerSelection").hide();
     });
+
 });
