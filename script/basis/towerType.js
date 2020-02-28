6131ef4d61163bb25760f483a60dfc496b7c73d0
kftsc <kftsc3@gmail.com>
2020-02-28 14:56:01 -0500

    add timer in Tower and add cooldown

--- script/basis/towerType.js
+++ script/basis/towerType.js
@@ -1,5 +1,5 @@
 const towerType = {
     // tower type and [typeName, damage, attack_cd, cost, range_radius]
-    ARCHER: ['archer', 10, 1, 100, 10],
-    FROZE: ['froze', 10, 2, 200, 5]
+    ARCHER: ['archer', 10, 2, 100, 10],
+    FROZE: ['froze', 10, 3, 200, 5]
 }
