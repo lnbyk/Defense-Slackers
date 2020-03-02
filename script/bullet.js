9b411e63de5c2de88faaf2573ad28f04b43c064b
Letian Zhang <zltian@udel.edu>
2020-03-02 15:06:38 -0500

    Bullet Prototype

--- script/bullet.js
+++ script/bullet.js
@@ -1,7 +1,12 @@
 class Bullet extends MovingObject {
-    constructor() {
+    constructor(px, py, type, target) {
         /* initilize the type, velocity, acceleration
             damage, attack_target*/
+            super(px, py);
+            this.type = type;
+            this.target = target;
+            console.log("TARGET" + this.target)
+            
     }
 
     collision() {
