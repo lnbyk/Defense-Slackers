3c35474f422f291dd277b6776d60a6b8d981f2d3
lnbyk <lnbyk@udel.edu>
2020-03-06 04:51:52 +0000

    adding setting menu for game

--- script/game.js
+++ script/game.js
@@ -1,5 +1,9 @@
 class Game {
     constructor() {
+        $("[id^='enemy']").remove();
+        $("[id^='tBtn']").attr('name', 'pit');
+        $("[id^='towerImg']").remove();
+        $("[id^='bullet']").remove();
         self = this;
         this.game_state = gameState.MAINMENU;
         console.log(this.game_state);
@@ -8,6 +12,8 @@ class Game {
     /*set up game
     call it when click the start botton*/
     setUp() {
+        $("[id^='enemy']").remove();
+        // clear enemy before 
         let self = this;
         this.game_state = gameState.PLAY;
         console.log(this.game_state);
@@ -16,40 +22,41 @@ class Game {
         this.tower_list = new Array();
         this.enemy_list = new Array();
         /* set interval and call update */
-        this.enemy_list.push(new Enemy(0, 200, enemyType.TANK,this.enemy_list.length));
-        setInterval(function(){self.update()}, 30); 
+        this.enemy_list.push(new Enemy(0, 200, enemyType.TANK, this.enemy_list.length));
+        setInterval(function () {
+            self.update()
+        }, 30);
     }
 
     /* */
     update() {
-        this.enemy_list.forEach(function(item) {
-            item.update();
-        });
         var self = this;
         switch (this.game_state) {
             case gameState.PLAY:
                 //console.log(this.game_state + " and " + "updating");
-                 //update each tower
-                this.tower_list.forEach(function(tower) {
-                   tower.update(self.enemy_list);
+                //update each tower
+                this.enemy_list.forEach(function (item) {
+                    item.update();
                 });
-        
+                this.tower_list.forEach(function (tower) {
+                    tower.update(self.enemy_list);
+                });
                 break;
             case gameState.PAUSE:
                 console.log(this.game_state);
                 break;
         }
-        
+
     }
 
-    buildTower(px ,py, typeS) {
+    buildTower(px, py, typeS) {
         //console.log(typeS);
         switch (typeS) {
             case '#0':
-                this.tower_list.push(new Tower(px,py,towerType.ARCHER));
+                this.tower_list.push(new Tower(px, py, towerType.ARCHER));
                 break;
             case '#1':
-                this.tower_list.push(new Tower(px,py,towerType.FROZE));
+                this.tower_list.push(new Tower(px, py, towerType.FROZE));
                 break;
             case '#2':
                 console.log("build tower #2, need define towerType");
@@ -58,12 +65,12 @@ class Game {
                 console.log("build tower #3, need define towerType");
                 break;
         }
-        
+
         console.log("tower list length: " + this.tower_list.length);
     }
 
     createEnemy() {
-        
+
     }
 
     /* the following four function just simply change the game_state*/
@@ -76,10 +83,10 @@ class Game {
     }
 
     pause() {
-
+        this.game_state = gameState.PAUSE;
     }
 
     resume() {
-
+        this.game_state = gameState.PLAY;
     }
 }
